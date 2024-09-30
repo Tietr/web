db1e8a41e1a2f9835bcf41b815d6e926e637a462
Tietr <2339047266@qq.com>
2024-09-30 16:10:38 +0800

    Init

---
+++ study_backend/src/main/java/com/learn/StudyBackendAndFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyBackendAndFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyBackendAndFrontendApplication.class, args);
+    }
+
+}
