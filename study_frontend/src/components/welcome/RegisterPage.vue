5c4b80d1514a7a96835b9009b1a8b33440b82322
Tietr <2339047266@qq.com>
2024-10-11 15:41:43 +0800

    注册页面表单验证

--- study_frontend/src/components/welcome/RegisterPage.vue
+++ study_frontend/src/components/welcome/RegisterPage.vue
@@ -1,7 +1,8 @@
 <script setup>
 import {Lock, User, Message, ChatLineRound} from "@element-plus/icons-vue";
 import router from "@/router/index.js";
-import {reactive} from "vue";
+import {reactive,ref} from "vue";
+
 const form = reactive({
   username: "",
   password: "",
@@ -9,6 +10,65 @@ const form = reactive({
   email:"",
   code:""
 })
+const validName = (rule,value,callback)=>{
+  if (value===''){
+    callback(new Error("请输入用户名"));
+  }else if(!/[\u4e00-\u9fa5a-zA-Z0-9]+/.test(value)){
+    callback(new Error("用户名不能包含特殊字符"))
+  }else if(2 > (String)(value).length || 8 < (String)(value).length) {
+    callback(new Error("用户名长度应该在2个到8个字符之间"))
+  }else {
+    callback()
+  }
+}
+const validPassword = (rule,value,callback)=>{
+  if (value===''){
+    callback(new Error("请输入密码"))
+  }else if(!/^[^\u4e00-\u9fa5]+$/.test(value)){
+    callback(new Error("密码不能包含中文字符"))
+  }else if(6 > (String)(value).length || 20 < (String)(value).length) {
+    callback(new Error("密码长度应该在6到20个字符之间"))
+  }else{
+    callback()
+  }
+}
+const validPasswordCheck = (rule, value, callback) => {
+  if (value === '') {
+    callback(new Error('请再次输入密码'))
+  } else if (value !== form.password) {
+    callback(new Error("两次密码不一致"))
+  } else {
+    callback()
+  }
+}
+const isEmailValid = ref(false)
+const onValidate = (prop,isValid) => {
+  if(prop === 'email'){
+    isEmailValid.value = isValid;
+  }
+}
+
+const rules = {
+  username: [
+    { validator: validName, trigger:['blur','change']}
+  ],
+  password: [
+    { validator:validPassword, trigger:['blur','change']},
+  ],
+  password_repeat: [
+    {validator:validPasswordCheck, trigger:['blur','change'] },
+  ],
+  email: [
+    {required: true, message: '请输入邮箱地址', trigger:'blur',},
+    {type: 'email', message: '请输入正确的邮箱地址', trigger: ['blur', 'change'],},
+  ],
+  code:[
+
+  ]
+}
+
+
+
 </script>
 
 <template>
@@ -18,40 +78,50 @@ const form = reactive({
       <div style="font-size: 14px;color: grey">欢迎注册，请在下方填写您的相关信息</div>
     </div>
     <div style="margin-top: 50px;">
-      <el-input v-model="form.username" type="text" placeholder="用户名">
-        <template #prefix>
-          <el-icon ><User /></el-icon>
-        </template>
-      </el-input>
-      <el-input v-model="form.password" type="password" placeholder="输入密码" style="margin-top: 10px">
-        <template #prefix>
-          <el-icon><Lock /></el-icon>
-        </template>
-      </el-input>
-      <el-input v-model="form.password_repeat" type="password" placeholder="重复输入密码" style="margin-top: 10px">
-        <template #prefix>
-          <el-icon><Lock /></el-icon>
-        </template>
-      </el-input>
-      <el-input v-model="form.email" type="text" placeholder="电子邮件" style="margin-top: 10px">
-        <template #prefix>
-          <el-icon><message /></el-icon>
-        </template>
-      </el-input>
-      <div style="margin-top: 10px; ">
-        <el-row :gutter="7">
-          <el-col :span="16" >
-            <el-input v-model="form.code" type="text" placeholder="请输入验证码">
-              <template #prefix>
-                <el-icon><ChatLineRound /></el-icon>
-              </template>
-            </el-input>
-          </el-col>
-          <el-col :span="8">
-            <el-button type="primary" >获取验证码</el-button>
-          </el-col>
-        </el-row>
-      </div>
+      <el-form :model="form" :rules="rules" @validate="onValidate">
+        <el-form-item prop="username">
+          <el-input v-model="form.username" type="text" placeholder="用户名">
+            <template #prefix>
+              <el-icon ><User /></el-icon>
+            </template>
+          </el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input v-model="form.password" type="password" placeholder="输入密码" >
+            <template #prefix>
+              <el-icon><Lock /></el-icon>
+            </template>
+          </el-input>
+        </el-form-item>
+        <el-form-item prop="password_repeat">
+          <el-input v-model="form.password_repeat" type="password" placeholder="重复输入密码" >
+            <template #prefix>
+              <el-icon><Lock /></el-icon>
+            </template>
+          </el-input>
+        </el-form-item>
+        <el-form-item prop="email">
+          <el-input v-model="form.email" type="text" placeholder="电子邮件">
+            <template #prefix>
+              <el-icon><message /></el-icon>
+            </template>
+          </el-input>
+        </el-form-item>
+        <el-form-item prop="code">
+          <el-row :gutter="7">
+            <el-col :span="16" >
+              <el-input v-model="form.code" type="text" placeholder="请输入验证码">
+                <template #prefix>
+                  <el-icon><ChatLineRound /></el-icon>
+                </template>
+              </el-input>
+            </el-col>
+            <el-col :span="8">
+              <el-button type="primary" :disabled="!isEmailValid" >获取验证码</el-button>
+            </el-col>
+          </el-row>
+        </el-form-item>
+      </el-form>
       <div style="margin-top: 40px; ">
         <el-button style="width: 270px" type="success">立即注册</el-button>
       </div>
@@ -64,5 +134,6 @@ const form = reactive({
 </template>
 
 <style scoped>
+
 
 </style>
--- study_frontend/src/views/WelcomeView.vue
+++ study_frontend/src/views/WelcomeView.vue
@@ -6,20 +6,23 @@
   <div style="width:100vw;height:100vh;overflow:hidden;display: flex">
     <div style="flex: 1;background-color: aquamarine">
       <el-image
-          style="width:100%;height:100%;"
-          fit="cover"
-          src="https://q2.itc.cn/q_70/images03/20240127/12b80fe65bfe469a946f2f13a5a22d38.jpeg"/>
+        style="width:100%;height:100%;"
+        fit="cover"
+        src="https://q2.itc.cn/q_70/images03/20240127/12b80fe65bfe469a946f2f13a5a22d38.jpeg"/>
     </div>
     <div class="welcome-title">
-
-
       <div>个人学习开发项目模版</div>
     </div>
-    <div style="width: 400px;background-color: white" >
-      <Transition name="el-zoom-in-center">
-        <router-view/>
-      </Transition>
+    <div style="width: 400px;background-color: white;z-index: 1" >
+      <router-view v-slot="{ Component }">
+        <transition name="el-zoom-in-center" mode="out-in">
+          <component :is="Component" />
+        </transition>
+      </router-view>
     </div>
+
+
+
   </div>
 
 </template>
