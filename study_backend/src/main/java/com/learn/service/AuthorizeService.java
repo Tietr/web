aa3679ee302c42e019fb3f84b1ea9708b40e5f8f
Tietr <2339047266@qq.com>
2024-10-12 10:27:22 +0800

    无

--- study_backend/src/main/java/com/learn/service/AuthorizeService.java
+++ study_backend/src/main/java/com/learn/service/AuthorizeService.java
@@ -1,6 +1,5 @@
 package com.learn.service;
 
-import jakarta.websocket.Session;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 public interface AuthorizeService extends UserDetailsService {
